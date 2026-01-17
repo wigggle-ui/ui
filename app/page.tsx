f08005f0c088f70e5c520426f93e47967d6fcbb0
sarthakkeche <kechesarthak412@gmail.com>
2026-01-17 16:58:43 +0530

    comment removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Features from "@/components/features";
 import Hero from "@/components/hero";
-// Only import the requested component
 import SocialTestimonials from "@/components/social-testimonials"; 
 
 export default function Home() {
@@ -8,8 +7,6 @@ export default function Home() {
     <div className="flex flex-col gap-12">
       <Hero />
       <Features />
-      
-      {/* The Issue only asked for this 👇 */}
       <SocialTestimonials />
     </div>
   );
